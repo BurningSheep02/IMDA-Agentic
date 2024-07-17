9f94e2be0cc29e93cbd56cd497672d87e00b40c8
Chen Peiran <peiran18@Chens-MacBook-Pro.local>
2024-07-17 11:14:11 +0800

    working chat ui

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,53 +1,40 @@
-// import { useState } from 'react'
-import { 
-  BrowserRouter as Router, Routes, 
-  Route 
-} from "react-router-dom";
-import logo from '/star.svg'
-import './App.css'
-import Chat from './pages/chat' // import the Chat component from src/pages/
-import MyButton from './components/button'
-
-// const App = () => { 
-//   return ( 
-//       <div className="app"> 
-//           <Router> 
-//               <MyButton to="chat" /> 
-//               <Routes> 
-//                   <Route path="/chat" element={<Chat />} /> 
-//               </Routes> 
-//           </Router> 
-//       </div> 
-//   ) 
-// } 
+import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Chat from './Components/Chat';
+import logo from './assets/IMDA.svg';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={logo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>IMDAi</h1>
-      <div className="card">
-          <Router> 
-              <MyButton to="chat" /> 
-              <Routes> 
-                  <Route path="/chat" element={<Chat />} /> 
-              </Routes> 
-          </Router>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="App">
+            <div className="navbar">
+                <div className="IMDAlogo">
+                    <a href="https://react.dev" target="_blank">
+                        <img src={logo} className="logo imda" alt="IMDA logo" />
+                    </a>
+                </div>
+                <div className='nav-title'>
+                    <Link to="/" className='nav-link'>
+                        Home
+                    </Link>
+                    <Link to="/about" className='nav-link'>
+                        About
+                    </Link>
+                    <Link to="/chat" className='nav-link'>
+                        Chat
+                    </Link>
+                </div>
+            </div>
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/chat" element={<Chat />} />
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
